34b160121b6666c030ec07d725824b2f7ccc36e4
Kirll <k.svezhinov@iqdev.digital>
2021-10-25 23:32:45 +0500

    api

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,55 @@
-const swagger = require('./openapi');
-const express = require('express');
+const swagger = require("./openapi");
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 
-const swaggerUI = require('swagger-ui-express')
+const { shopsData, shopsIems, detailData } = require("./data");
+const swaggerUI = require("swagger-ui-express");
 
-
-app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swagger))
-
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swagger));
 
 const PORT = 3001;
 app.use(cors());
 app.use(express.json());
 
-
-
-const obj = {
-  name:'asdasd',
-  age:'17',
-  work:'IT'
+const findShopBySlug = (slug) =>{
+  return shopsData.find(item => item.slug === slug);
 }
-app.get('/', function (req, res) {
-  res.send(obj);  
+
+
+app.get("/api/catalog/", function (req, res) {
+  res.send(shopsData);
+});
+app.get("/api/catalog/:shop", function (req, res) {
+  const shop = findShopBySlug(req.params.shop)
+  res.send({
+    items:shopsIems,
+    shop
+  })
+  res.send(shopsIems);
+});
+app.get("/api/catalog/:shop/:item", function (req, res) {
+  
+  const item = req.params.item
+  const shop = findShopBySlug(req.params.shop)
+  res.send({
+    item:{
+      ...detailData[`${item}`] 
+    },
+    shop
+  })
+  res.send(detailData[`${item}`]);
 });
 
-const start = () =>{
+
+
+const start = () => {
   try {
     app.listen(PORT, () => {
-      console.log(`Server app listening at http://localhost:${PORT}`)
-    })
+      console.log(`Server app listening at http://localhost:${PORT}`);
+    });
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
-}
+};
 start();
